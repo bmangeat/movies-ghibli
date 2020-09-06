fb8288bd6a74f87d7038de0b9ad4457a2c2e0a54
bmangeat <bmangeat@users.noreply.github.com>
2020-09-06 09:36:27 +0000

    deploy: 0016dbb9287d72ef8c79c37ab3de0e2cedf4d7d8

---
+++ precache-manifest.f83dabfa66073d7f306ad19b9b443771.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d72d674cf09bccd68055454be80d5c17",
+    "url": "/react-movies-ghibli/index.html"
+  },
+  {
+    "revision": "ef2b597c3fb51a296cbd",
+    "url": "/react-movies-ghibli/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4d4e6276099be5cc4cd1",
+    "url": "/react-movies-ghibli/static/js/2.05fbc689.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies-ghibli/static/js/2.05fbc689.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2b597c3fb51a296cbd",
+    "url": "/react-movies-ghibli/static/js/main.ea68ec3a.chunk.js"
+  },
+  {
+    "revision": "6c0fd4f4d3e626eace62",
+    "url": "/react-movies-ghibli/static/js/runtime-main.80994919.js"
+  }
+]);
