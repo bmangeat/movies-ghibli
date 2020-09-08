0f56a6b58fc5fe5cc5b03938cfe8797549544089
bmangeat <bmangeat@users.noreply.github.com>
2020-09-08 10:55:05 +0000

    deploy: e5787e2fc45d216678b9f1b50011961a75567aff

---
+++ precache-manifest.e3da37c02a8bcf3ac370accbfcf8a272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ce564b1d5758cbfdff60164a8e27a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a09a614b1597c0ddbb3",
+    "url": "/static/css/main.7ad1eca1.chunk.css"
+  },
+  {
+    "revision": "5ad1d40522d341a3f024",
+    "url": "/static/js/2.d377b65e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d377b65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a09a614b1597c0ddbb3",
+    "url": "/static/js/main.de9fdf7d.chunk.js"
+  },
+  {
+    "revision": "10dd3407c607655f9995",
+    "url": "/static/js/runtime-main.aaa43a26.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-movies-ghibli/precache-manifest.c5c92fdd854ec58818ef4b546017eacc.js"
+  "/precache-manifest.e3da37c02a8bcf3ac370accbfcf8a272.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movies-ghibli/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
